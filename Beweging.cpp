99dd508450576f3a4fe23c4a26fee6d2474fd14b
ThibeVanOrshaegen <thibe.van@hotmail.com>
2023-12-03 11:16:22 +0100

    Toevoeging Wave, Beweging aangepast, images toegevoegd

--- Beweging.cpp
+++ Beweging.cpp
@@ -1,19 +1,48 @@
-#ifndef BEWEGING_H
-#define BEWEGING_H
-
-#include <SFML/Graphics.hpp>
+#include "Beweging.h"
 namespace Towerdefense
 {
-    class Beweging {
-    private:
-        float xVelocity;
-        float yVelocity;
-        float originalYVelocity;
+    Beweging::Beweging(float xVel, float yVel) : xVelocity(xVel), yVelocity(yVel), originalYVelocity(yVel) {}
 
-    public:
-        Beweging(float xVel, float yVel);
 
-        void move(sf::Vector2f& position);
+    void Beweging::move(sf::Vector2f& position) {
+        if (position.x < 170 && (position.y > 100 && position.y < 420))
+        {
+            position.x += xVelocity;
+        }
+        if ((position.x >= 170 && position.x < 200) && position.y > 172)
+        {
+            position.y -= yVelocity;
+        }
+        if (position.x < 412 && position.y < 172)
+        {
+            position.x += xVelocity;
+        }
+        if ((position.x >= 412 && position.x <= 700) && position.y < 172)
+        {
+            yVelocity *= -1;
+        }
+        if (position.y < 490 && position.x >= 412)
+        {
+            position.y -= yVelocity;
+        }
+        if (position.x < 734 && position.y >= 490)
+        {
+            position.x += xVelocity;
+        }
+        if (position.x >= 734 && position.y >= 340)
+        {
+            yVelocity = originalYVelocity;
+            position.y -= yVelocity;
+        }
+
+        if (position.x >= 734 && position.y <= 340)
+        {
+            yVelocity = 0;
+
+            if (position.x <= 1000)
+            {
+                position.x += xVelocity;
+            }
+        }
     };
 }
-#endif 
