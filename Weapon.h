b77aec08332814ec14ddd0a1a2d90a058ef0762f
ThibeVanOrshaegen <thibe.van@hotmail.com>
2023-12-03 11:09:42 +0100

    Toevoeging Weapon.h

---
+++ Weapon.h
@@ -0,0 +1,14 @@
+#ifndef WEAPONS_H
+#define WEAPONS_H
+
+#include <SFML/Graphics.hpp>
+namespace Towerdefense
+{
+    class Weapon {
+    public:
+        virtual ~Weapon() = default;
+        virtual void fire(sf::Vector2f shooterPosition) = 0; //polymorfisme
+        virtual void update() = 0; //abstract
+    };
+}
+#endif
