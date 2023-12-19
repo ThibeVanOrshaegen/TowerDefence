d87e5a14e98928eeb21728f93ab2d46a219ddb32
Thibe <114075982+ThibeVanOrshaegen@users.noreply.github.com>
2023-12-19 23:22:44 +0100

    Update drawSpriteIfVisible.cpp

--- CPP_File/drawSpriteIfVisible.cpp
+++ CPP_File/drawSpriteIfVisible.cpp
@@ -4,9 +4,11 @@
 #define M_PI 3.14159265358979323846
 namespace Towerdefense
 {
+    // Function to draw a sprite if it is visible
     void drawSpriteIfVisible::drawSpriteVisible(sf::RenderWindow& window, sf::Sprite& sprite, sf::Vector2f shooterPosition, sf::Vector2f origin) {
         sprite.setOrigin(origin);
         //sprite.setRotation(rotation);
+        // Set the position of the sprite to the shooter's position
         sprite.setPosition(shooterPosition);
         window.draw(sprite);
     }
