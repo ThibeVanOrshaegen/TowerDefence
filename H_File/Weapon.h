5ff8941bb85bf7f55affa583e63aab046b943b4f
Thibe <114075982+ThibeVanOrshaegen@users.noreply.github.com>
2023-12-19 23:07:41 +0100

    Update Weapon.h

--- H_File/Weapon.h
+++ H_File/Weapon.h
@@ -6,8 +6,14 @@ namespace Towerdefense
 {
     class Weapon {
     public:
+
+        // Virtual destructor to enable proper polymorphism
         virtual ~Weapon() = default;
+
+        // Pure virtual function for firing the weapon, promoting polymorphism
         virtual void fire(sf::Vector2f& shooterPosition,float limit_one, float limit_two, float limit_three, float limit_four) = 0; //polymorfisme
+        
+        // Pure virtual function for updating the weapon, promoting polymorphism
         virtual void update(float directionX, float directionY) = 0;
     };
 }
