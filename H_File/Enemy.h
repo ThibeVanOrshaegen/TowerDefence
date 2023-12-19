a506021ea7e6027d9321aee095e6a70f2dc24ac2
Thibe <114075982+ThibeVanOrshaegen@users.noreply.github.com>
2023-12-19 23:02:26 +0100

    Update Enemy.h

--- H_File/Enemy.h
+++ H_File/Enemy.h
@@ -7,25 +7,37 @@
 
 namespace Towerdefense
 {
-
+    // Declaration of the Enemy class, derived from HealthBar
     class Enemy : public HealthBar {
     public:
-
+        // TextureManager for handling textures
         TextureManager textureManager;
+
+        // Beweging for handling movement
         Beweging beweging;
+
+        // Sprite representing the enemy
         sf::Sprite spriteEnemy;
 
+        // Virtual destructor for proper polymorphic behavior
         virtual ~Enemy() = default;
+
+        // Constructor for creating an Enemy with specified parameters
         Enemy(sf::Vector2f initialPosition, sf::Vector2f size, unsigned char RandommaxHealth, int xVelocity, int  yVelocity);
 
-        // Copy constructor
+        // Copy constructor for creating a copy of an existing Enemy
         Enemy(const Enemy& other);
 
+        // Method to update the movement and health of the enemy
         void updateMoveHealth();
-        void updatePosition();
-        void updateHealthBar();
 
+        // Method to update the position of the enemy
+        void updatePosition();
+
+        // Method to update the health bar of the enemy
+        void updateHealthBar();
 
     };
 }
+
 #endif
