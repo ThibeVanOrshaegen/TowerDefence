8a517cf81710f8cb930d767d6a7d28d3f69bc33e
Thibe <114075982+ThibeVanOrshaegen@users.noreply.github.com>
2023-12-19 23:14:41 +0100

    Update HandleIntersectionAndDamage.h.cpp

--- CPP_File/HandleIntersectionAndDamage.h.cpp
+++ CPP_File/HandleIntersectionAndDamage.h.cpp
@@ -3,10 +3,11 @@
 
 namespace Towerdefense
 {
-
+    // Method definition to handle intersections between a weapon and an enemy and apply damage
     void HandleIntersectionAndDamage::handleIntersectionsAndDamage(sf::Sprite& weaponSprite, sf::Sprite& robotSprite, HealthBar& healthBar) {
         if (weaponSprite.getGlobalBounds().intersects(robotSprite.getGlobalBounds())) {
             healthBar.applyDamage();
+            // Subtract a specific amount of health from the health bar
             healthBar.subtractHealth(4);
 
             std::vector<std::string> linesToWrite = { "Enemy has been hit" };
