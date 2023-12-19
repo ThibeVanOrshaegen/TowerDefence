b6f61979c41a34198a60b56e96428907b12d87ce
Thibe <114075982+ThibeVanOrshaegen@users.noreply.github.com>
2023-12-19 23:23:21 +0100

    Update main.cpp

--- CPP_File/main.cpp
+++ CPP_File/main.cpp
@@ -1,13 +1,9 @@
 #include <SFML/Graphics.hpp>
-
 #include "Game.h"
-
-
-using namespace Towerdefense;
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(1180, 720), "My Program");
-    Game game;
+    Towerdefense::Game game;
     game.run(window);
 
     return 0;
