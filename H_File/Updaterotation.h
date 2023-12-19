94f8c4882a071367fc6ee038b96f170cdd9ec299
Thibe <114075982+ThibeVanOrshaegen@users.noreply.github.com>
2023-12-19 23:07:24 +0100

    Update Updaterotation.h

--- H_File/Updaterotation.h
+++ H_File/Updaterotation.h
@@ -14,18 +14,25 @@ namespace Towerdefense {
         TextureManager textureManager;
 
     public:
+
+        // Method to update the rotation of a cannon shooter based on an enemy's position
         void updateShooterRotationCanon(Canon* canonplaats, Enemy* snel, int Graden);
+        
+        // Method to update the rotation of a laser shooter based on an enemy's position
         void updateShooterRotationLaser(Laser* laserplaats, Enemy* traag, int Graden);
+        
+        // Method to update the rotation of a cannon bullet based on an enemy's position
         void updateShooterRotationCanonkogel(Canonshooting* canonplaats, Enemy* snel, int Graden);
+        
+        // Method to update the rotation of a laser bullet based on an enemy's position
         void updateShooterRotationLaserkogel(Lasershooting* laserplaats, Enemy* traag, int Graden);
 
+        // Getter method to check if ammo is turned on
         bool getAmmo() const { return TurnAmmoOn; }
 
     private:
 
         static bool TurnAmmoOn;
     };
-
-} // namespace Towerdefense
-
-#endif // UPDATEROTATION_H
+}
+#endif
