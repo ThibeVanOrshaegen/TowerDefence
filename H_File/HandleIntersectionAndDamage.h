428d23b75b73ffbff1618a24a0dd86d7c348a727
Thibe <114075982+ThibeVanOrshaegen@users.noreply.github.com>
2023-12-19 23:04:09 +0100

    Update HandleIntersectionAndDamage.h

--- H_File/HandleIntersectionAndDamage.h
+++ H_File/HandleIntersectionAndDamage.h
@@ -1,15 +1,16 @@
-#ifndef HANDLE_INTERSECTION_AND_DAMAGE_H
-#define HANDLE_INTERSECTION_AND_DAMAGE_H
-
-
 #include <SFML/Graphics.hpp>
 #include "HealthBar.h" 
 namespace Towerdefense
 {
+    // Declaration of HandleIntersectionAndDamage class, derived from HealthBar
+
     class HandleIntersectionAndDamage : public HealthBar {
     public:
+        // Static method to handle intersections and damage between weapon and robot
+
         static void handleIntersectionsAndDamage(sf::Sprite& weaponSprite, sf::Sprite& robotSprite, HealthBar& healthBar);
     };
 }
 #endif 
+
 
