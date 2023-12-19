aba854c6b20ada40fda43f0e85245f695977ab84
Thibe <114075982+ThibeVanOrshaegen@users.noreply.github.com>
2023-12-19 22:56:51 +0100

    Update CurrentMoney.h

--- H_File/CurrentMoney.h
+++ H_File/CurrentMoney.h
@@ -1,4 +1,3 @@
-// CurrentMoney.h
 #ifndef CURRENT_MONEY_H
 #define CURRENT_MONEY_H
 
@@ -8,21 +7,34 @@
 
 namespace Towerdefense
 {
+    // Forward declaration of the Game class
     class Game;
+
+    // CurrentMoney class inherits from Gold class
     class CurrentMoney : public Gold
     {
     private:
+        // TextureManager object for handling textures
         TextureManager textureManager;
+
+        // Private constructor to initialize CurrentMoney with default values
         CurrentMoney() : currentmoney() {}
-        CurrentMoney(int currentgold = 10);  
+
+        // Constructor with an optional initial amount of gold
+        CurrentMoney(int currentgold = 10);
+
+        // Override functions for sum and subtract operations
         int sum(int getaleen, int getaltwee) override;
         int subtract(int getaleen, int getaltwee) override;
 
+        // Granting friendship to the Game class to access private members
         friend class Game;
 
+        // Private member variable to store the current amount of money
         int currentmoney;
+
     public:
-        
+        // Public method to get the current amount of money
         const int& getCurrentMoney() const { return currentmoney; }
     };
 }
