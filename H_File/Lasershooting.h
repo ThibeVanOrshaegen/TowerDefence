f850c304713feea9467ae9ade95d28686331260f
Thibe <114075982+ThibeVanOrshaegen@users.noreply.github.com>
2023-12-19 23:05:10 +0100

    Update Lasershooting.h

--- H_File/Lasershooting.h
+++ H_File/Lasershooting.h
@@ -7,28 +7,31 @@
 
 namespace Towerdefense
 {
-
+    // Declaration of the Lasershooting class, derived from Weapon
     class Lasershooting : public Weapon {
     private:
         sf::Vector2f laserstraalPosition;
         TextureManager textureManager;
         sf::Vector2f direction;
         const unsigned char LaserSpeed = 20;
+
+        // Static flag indicating if the laser is out of range
         static bool outOfRange;
 
     public:
 
         sf::Sprite spriteLaserKogeltwee;
 
+        // Constructors for creating a Lasershooting object with or without an initial position
         Lasershooting();
         Lasershooting(const sf::Vector2f& initialPosition);
 
+        // Override methods for firing and updating the laser
         void fire(sf::Vector2f& shooterPosition, float limit_one, float limit_two, float limit_three, float limit_four) override;
         void update(float directionX, float directionY) override;
         void updateprobeer();
 
         bool getOutofRange() const { return outOfRange; }
-
 
     };
 }
