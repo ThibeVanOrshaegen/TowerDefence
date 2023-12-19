3e6d7bb3d895d3ccfcade8f82adf8f605f2c4742
Thibe <114075982+ThibeVanOrshaegen@users.noreply.github.com>
2023-12-19 23:12:28 +0100

    Update CurrentMoney.cpp

--- CPP_File/CurrentMoney.cpp
+++ CPP_File/CurrentMoney.cpp
@@ -11,9 +11,10 @@ namespace Towerdefense
     {
         try
         {
+            // Calculate the sum of two values
             currentmoney = getaleen + getaltwee;
 
-            
+            // Check if the result is negative and throw an exception if so
             if (currentmoney < 0)  
             {
                 
@@ -25,26 +26,27 @@ namespace Towerdefense
         }
         catch (const std::exception& e)
         {
-            
+            // Handle the exception by logging it to a file
             std::ofstream errorFile("error_log_twee.txt", std::ios_base::app);
 
-           
+            // Check if the error log file is open and write the exception details
             if (errorFile.is_open())
             {
-                
+                // Check if the error log file is open and write the exception details
                 errorFile << "Exception caught while initializing the game: " << e.what() << std::endl;
                 
                 errorFile.close();
             }
             else
             {
-                
+                // Print an error message to the standard error stream if the file cannot be opened
                 std::cerr << "Failed to open the error log file!" << std::endl;
             }
-
+            // Re-throw the exception after logging
             throw;
         }
     }
+    // Method definition to subtract one value from another
     inline int CurrentMoney::subtract(int getaleen, int getaltwee)
     {
         currentmoney = getaleen - getaltwee;
